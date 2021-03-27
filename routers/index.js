1563a9b893e5d6fff3d0a28b846cf52f336ad47f
Vinicius <viniciuz.lol5@gmail.com>
2021-03-27 14:47:35 -0300

    commit

--- routers/index.js
+++ routers/index.js
@@ -2,21 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/',(request,response)=>{
-    let nome = request.query.nome;
-    let sobrenome = request.query.sobrenome;
-
-    let resposta = {nomeCompleto:nome+' '+sobrenome};
-    response.json(resposta);
-    //response.send('Hello World Node! 2.0');
-});
-
-router.get('/sobre',(request,response)=>{
-    response.send('Página Sobre!');
-});
-
-router.get('/posts/:id',(request,response)=>{
-    let id = request.params.id;
-    response.send('Id do post: '+id);
+    response.render('home', {
+        'nome':'Vinicius',
+        'idade': 90
+    });
 });
 
 module.exports = router;
+
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.DATABASE,{ useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 mongoose.connection.on('error',(error)=>{
     console.error("ERRO +"+error.message);
-})
+});
 
 app.set('port', process.env.PORT || 3333);
 
