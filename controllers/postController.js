8aa47eeede985dce9499489eadbd1e627c4ee5f0
ViniciusMorais10 <viniciuz.lol5@gmail.com>
2021-03-29 22:36:14 -0300

    29/03 curso

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
+const slug = require('slug');
 const Post = mongoose.model('Post');
 
 exports.add = ((request,response) =>{
     response.render('postAdd');
 });
 
+exports.view=(async (request,response)=>{
+    const post = await Post.findOne({ slug:request.params.slug });
+    response.render('view',{ post });
+});
+
 exports.addAction = (async(request,response)=>{
+    request.body.tags = request.body.tags.split(',').map(tag=>tag.trim());
+
     const post = new Post(request.body);
+
     
     try{
     await post.save();
@@ -24,16 +33,25 @@ exports.edit = (async (request,response)=>{
 });
 
 exports.editAction = (async(request,response)=>{
-   //procurar o item enviado
-   const post = await Post.findOneAndUpdate(
+    request.body.slug = slug(request.body.title,{lower:true});
+    request.body.tags = request.body.tags.split(',').map(tag=>tag.trim());
+    try{
+       //procurar o item enviado
+    const post = await Post.findOneAndUpdate(
        { slug:request.params.slug },
         request.body,
+        
         {
             new:true,//Retornar novo item atualizado
             runValidators:true // Rodar as validações do Add. required, e as demais
 
         }
     ); 
+    }catch(e){
+        request.flash('error','Erro:'+ e.message);
+        return response.redirect('/post/'+request.params.slug+'/edit');
+    };
+
     request.flash('sucess', 'Post Atualizado com sucesso! ');
     response.redirect('/');
 
--- routers/index.js
+++ routers/index.js
@@ -7,10 +7,15 @@ const router = express.Router();
 router.get('/',homeController.userMiddleware, homeController.index);
 router.get('/users/login',userController.login);
 router.get('/users/register',userController.register);
+
 router.get('/post/add',postController.add);
 router.post('/post/add',postController.addAction);
+
 router.get('/post/:slug/edit',postController.edit);
 router.post('/post/:slug/edit',postController.editAction);
+
+router.get('/post/:slug',postController.view); // colocar no final para nao dar conflito na url, por conta da slug. o JS executa em ordem, stilo delphi
+
 
 module.exports = router;
 
